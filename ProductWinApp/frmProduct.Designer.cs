dcec6c00e3625b1ecb25e5b8217a2d3047779b28
cotzuong2k3 <114601947+cotzuong2k3@users.noreply.github.com>
2023-10-20 15:02:17 +0700

    Disconnected

---
+++ ProductWinApp/frmProduct.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ProductWinApp
+{
+    partial class frmProduct
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvList = new DataGridView();
+            btnView = new Button();
+            btnViewCategories = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvList).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvList
+            // 
+            dgvList.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvList.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvList.Location = new Point(54, 135);
+            dgvList.Margin = new Padding(4, 5, 4, 5);
+            dgvList.Name = "dgvList";
+            dgvList.RowTemplate.Height = 25;
+            dgvList.Size = new Size(767, 230);
+            dgvList.TabIndex = 1;
+            // 
+            // btnView
+            // 
+            btnView.Location = new Point(118, 58);
+            btnView.Name = "btnView";
+            btnView.Size = new Size(205, 36);
+            btnView.TabIndex = 2;
+            btnView.Text = "View Products";
+            btnView.UseVisualStyleBackColor = true;
+            btnView.Click += btnView_Click;
+            // 
+            // btnViewCategories
+            // 
+            btnViewCategories.Location = new Point(449, 58);
+            btnViewCategories.Name = "btnViewCategories";
+            btnViewCategories.Size = new Size(205, 36);
+            btnViewCategories.TabIndex = 2;
+            btnViewCategories.Text = "View Categories";
+            btnViewCategories.UseVisualStyleBackColor = true;
+            btnViewCategories.Click += btnViewCategories_Click;
+            // 
+            // frmProduct
+            // 
+            AutoScaleDimensions = new SizeF(10F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(891, 616);
+            Controls.Add(btnViewCategories);
+            Controls.Add(btnView);
+            Controls.Add(dgvList);
+            Font = new Font("Segoe UI", 13F, FontStyle.Bold, GraphicsUnit.Point);
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "frmProduct";
+            Text = "frmProduct";
+            Load += frmProduct_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvList).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private DataGridView dgvList;
+        private Button btnView;
+        private Button btnViewCategories;
+    }
+}
---
+++ ProductWinApp/frmProduct.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProductWinApp
+{
+    public partial class frmProduct : Form
+    {
+        public frmProduct()
+        {
+            InitializeComponent();
+        }
+
+        private string GetConnectionString()
+        {
+
+            IConfiguration config = new ConfigurationBuilder()
+                            .SetBasePath(Directory.GetCurrentDirectory())
+                            .AddJsonFile("appsettings.json", true, true)
+                          .Build();
+            var strConnection = config["ConnectionStrings:MyStoreDB"];
+            return strConnection;
+
+
+        }
+
+
+
+        private void frmProduct_Load(object sender, EventArgs e)
+        {
+            DbProviderFactory factory = SqlClientFactory.Instance;
+
+            using DbConnection connection = factory.CreateConnection();
+
+            if (connection == null)
+            {
+                Console.WriteLine($"Unable to create the connection object.");
+                return;
+            }
+
+
+            connection.ConnectionString = GetConnectionString();
+            connection.Open();
+            string sql = "select ProductID, Name from Products; select * from categories;";
+            try
+            {
+
+
+                SqlDataAdapter adapter = new SqlDataAdapter(sql, GetConnectionString());
+                adapter.Fill(dsMyStore);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Get Data from Database");
+            }
+        }
+        DataSet dsMyStore = new DataSet();
+        private void btnViewCategories_TextChanged(object sender, EventArgs e)
+        {
+            dgvList.DataSource = dsMyStore.Tables[1];
+        }
+
+        private void btnView_Click(object sender, EventArgs e)
+        {
+            dgvList.DataSource = dsMyStore.Tables[0];
+        }
+
+        private void btnViewCategories_Click(object sender, EventArgs e)
+        {
+            dgvList.DataSource = dsMyStore.Tables[1];
+        }
+
+
+    }
+}
